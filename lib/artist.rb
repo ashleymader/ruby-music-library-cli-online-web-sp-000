3c3d53c1baf053dc08220290727398416bce2048
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-19 18:46:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@
 
 class Artist 
   attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
-  extend 
+  extend Concerns::Findable
   @@all = []
   def initialize(name)
     @name = name 
