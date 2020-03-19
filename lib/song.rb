721f0273c86eb18c48253dd95bca585171550112
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-19 18:43:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ class Song
   end
   
   def self.find_by_name(name)
-    song_return = @@all.select {|song| song.name == name}
+    @@all.detect {|song| song.name == name}
   end
 
-    
+  def 
 end 
